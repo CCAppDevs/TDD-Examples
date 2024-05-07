d7aa40495e7a1a451ca9e81606b95dcc18738b46
Jesse Harlan <jesse.harlan@centralia.edu>
2024-05-07 11:53:25 -0700

    started working on jesselinkedlist. need to fix suceeding test.

---
+++ LibraryTests/JesseLinkedListTests.cs
@@ -0,0 +1,23 @@
+﻿using ClassLibraryExample;
+
+namespace LibraryTests
+{
+    [TestClass]
+    public class JesseLinkedListTests
+    {
+        // Creating a singly linked list that accepts strings
+
+        [TestMethod]
+        public void LinkedListAcceptsStrings_ReturnsStringData()
+        {
+            // arrange
+            JesseLinkedList list = new JesseLinkedList();
+
+            // act
+            string result = "";
+
+            // assert
+            Assert.IsInstanceOfType(result, typeof(string));
+        }
+    }
+}
