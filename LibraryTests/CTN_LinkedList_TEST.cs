7a09e93b017f9a8a8d8e7ec3e42f1b95aca9f10b
GiftedR <kidsgamers@hotmail.com>
2024-05-07 11:53:57 -0700

    Created CTN_LinkedList & CTN_LinkedList_TEST, populated CTN_LinkedList_TEST with first test method

---
+++ LibraryTests/CTN_LinkedList_TEST.cs
@@ -0,0 +1,21 @@
+﻿using ClassLibraryExample;
+using System.Runtime.CompilerServices;
+
+namespace LibraryTests
+{
+    [TestClass]
+    public class CTN_LinkedList_TEST
+    {
+        [TestMethod]
+        public void LinkedList_Accepts_Strings_Returns_StringData()
+        {
+            //Arrange
+            CTN_LinkedList ll = new();
+            string result = string.Empty;
+            //Act
+
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(string));
+        }
+    }
+}
