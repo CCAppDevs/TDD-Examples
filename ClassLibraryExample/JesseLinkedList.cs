3b99d0736e248da1ce6f9aa23195713945d26fea
Jesse Harlan <jesse.harlan@centralia.edu>
2024-05-08 11:55:32 -0700

    In class example

--- ClassLibraryExample/JesseLinkedList.cs
+++ ClassLibraryExample/JesseLinkedList.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace ClassLibraryExample
 {
+    // Node class for a linked list item
+    public class Node
+    {
+        public string Data { get; set; }
+    }
+
+    // The linked list class itself
     public class JesseLinkedList
     {
+        public Node Head { get; set; }
+
+        public JesseLinkedList()
+        {
+            Head = new Node();
+        }
+
+        public string Get(int index)
+        {
+            return Head.Data;
+        }
+
+        public void Add(string item)
+        {
+            Head = new Node { Data = item };
+        }
     }
 }
--- LibraryTests/JesseLinkedListTests.cs
+++ LibraryTests/JesseLinkedListTests.cs
@@ -14,10 +14,27 @@ namespace LibraryTests
             JesseLinkedList list = new JesseLinkedList();
 
             // act
-            string result = "";
+            string result = list.Get(1);
 
             // assert
             Assert.IsInstanceOfType(result, typeof(string));
+        }
+
+        [TestMethod]
+        [DataRow("Toast")]
+        public void LinkedListAcceptsAnItem_HeadReturnsTheItem(string input)
+        {
+            // arrange
+            JesseLinkedList list = new JesseLinkedList();
+
+            // act
+                // add in some way to add the input item to the list
+                list.Add(input);
+
+            Node result = list.Head;
+
+            // assert
+            Assert.AreEqual(result, new Node() { Data = "Toast" });
         }
     }
 }
