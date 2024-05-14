7729ef098424c8885787d5ebf467dfe75e13a85d
GiftedR <kidsgamers@hotmail.com>
2024-05-14 11:41:27 -0700

    Changed Add -> PushFront, Added PushBack, Added GetListEnd

--- ClassLibraryExample/CTN_LinkedList.cs
+++ ClassLibraryExample/CTN_LinkedList.cs
@@ -1,12 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ClassLibraryExample
 {
+    public class CTN_Node
+    {
+        public string data { get; set; }
+        public CTN_Node? next { get; set; }
+        public CTN_Node(string nodedata) { data = nodedata; }
+        //public static CTN_Node operator +(CTN_Node oldnode, CTN_Node newnode) => oldnode.next = newnode;
+        //public static CTN_Node? operator ++(CTN_Node oldnode) => oldnode.next;
+    }
     public class CTN_LinkedList
     {
+        public CTN_Node? head { get; set; }
+
+        public CTN_LinkedList()
+        {
+            head = null;
+        }
+
+        public string Get(int index)
+        {
+            CTN_Node listPointer = head;
+            int counter = 0;
+
+            while (counter < index)
+            {
+                listPointer = listPointer.next;
+                counter++;
+            }
+
+            return listPointer.data;
+        }
+
+        public void PushFront(string input)
+        {
+            CTN_Node newnode = new CTN_Node(input);
+
+            newnode.next = head;
+            head = newnode;
+        }
+
+        public void PushBack(string input)
+        {
+            CTN_Node newNode = new CTN_Node(input);
+
+            if (head == null)
+            {
+                head = newNode;
+                return;
+            }
+
+            CTN_Node listEnd = GetListEnd();
+            listEnd.next = newNode;
+        }
+    
+        public CTN_Node GetListEnd()
+        {
+            CTN_Node headPointer = head;
+
+            while (headPointer.next != null)
+            {
+                headPointer = headPointer.next;
+            }
+
+            return headPointer;
+        }
     }
 }
--- LibraryTests/CTN_LinkedList_TEST.cs
+++ LibraryTests/CTN_LinkedList_TEST.cs
@@ -1,4 +1,5 @@
 ﻿using ClassLibraryExample;
+using Microsoft.VisualStudio.TestPlatform.Utilities;
 using System.Runtime.CompilerServices;
 
 namespace LibraryTests
@@ -7,15 +8,66 @@ namespace LibraryTests
     public class CTN_LinkedList_TEST
     {
         [TestMethod]
-        public void LinkedList_Accepts_Strings_Returns_StringData()
+        [DataRow("Toast")]
+        public void LinkedList_ACEEPTS_Strings_RETURNS_String(string input)
         {
             //Arrange
             CTN_LinkedList ll = new();
-            string result = string.Empty;
             //Act
-
+            ll.PushFront(input);
+            string result = ll.Get(0);
             //Assert
             Assert.IsInstanceOfType(result, typeof(string));
+        }
+
+        [TestMethod]
+        [DataRow("Toast")]
+        public void LinkedList_ACEEPTS_Item_RETURNS_HeadItem(string input)
+        {
+            //Arrange
+            CTN_LinkedList ll = new();
+            CTN_Node expected = new CTN_Node(input);
+            //Act
+            ll.PushFront(input);
+            CTN_Node result = ll.head;
+            //Assert
+            Assert.AreEqual(result.data, expected.data);
+        }
+
+        [TestMethod]
+        public void LinkedListCreatedHead_IS_Null()
+        {
+            //Arrange
+            CTN_LinkedList ll = new();
+
+            //Act
+            CTN_Node? result = ll.head;
+
+            //Assert
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        [DataRow("Juan", "Tooh", "Tree")]
+        [DataRow("Toast", "Jelly", "Crunchy PB")]
+        public void LinkedList_ACCEPTS_Strings_CHECKS_InRightPlaces(string one, string two, string three)
+        {
+            //Arrange
+            CTN_LinkedList ll = new();
+
+            //Act
+            ll.PushBack(one);
+            ll.PushBack(two);
+            ll.PushBack(three);
+
+            string? resultOne = ll.Get(0);
+            string? resultTwo = ll.Get(1);
+            string? resultThree = ll.Get(2);
+
+            //Assert
+            Assert.AreEqual(resultOne, one);
+            Assert.AreEqual(resultTwo, two);
+            Assert.AreEqual(resultThree, three);
         }
     }
 }
