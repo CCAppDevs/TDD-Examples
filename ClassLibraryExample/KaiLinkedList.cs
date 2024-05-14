f3b930495450e3cb6e976e505140e4e3cb6d9fa0
Khai Ha <Khaiha265@gmail.com>
2024-05-14 13:03:43 -0700

    Pushing it

---
+++ ClassLibraryExample/KaiLinkedList.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryExample
+{
+    // Node class for a linked list item
+    public class Node
+    {
+        public string Data { get; set; }
+        public Node? Next { get; set; }
+
+
+        public Node(string data)
+        {
+            Data = data;
+        }
+    }
+    // The linked list class itself
+    public class KaiLinkedList
+    {
+        public Node? Head { get; set; }
+
+        public KaiLinkedList()
+        {
+            Head = null;
+        }
+
+        public string Get(int index)
+        {
+            Node node = Head;
+            int counter = 0;
+
+            //search through node list for given index. (if it exist)
+            while (counter != index)
+            {
+                node = node.Next;
+                counter++;
+
+            }
+
+            return node.Data;
+        }
+
+        public void AddToBack(string item)
+        {
+            // create a new node to hold our new item
+            Node newNode = new Node(item);
+
+            if (Head == null)
+            {
+                Head = newNode;
+                return;
+            }
+
+            Node temp = FindEndOfList();
+
+            temp.Next = newNode;
+        }
+
+        public Node FindEndOfList()
+        {
+            Node position = Head;
+
+            while (position.Next != null)
+            {
+                position = position.Next;
+            }
+
+            return position;
+        }
+        public void AddToFront(string item)
+        {
+            Node newNode = new Node(item);
+
+            newNode.Next = Head;
+            Head = newNode;
+        }
+
+    }
+}
---
+++ LibraryTests/KaiLinkedListTest.cs
@@ -0,0 +1,105 @@
+﻿using ClassLibraryExample;
+
+namespace LibraryTests
+{
+    [TestClass]
+    internal class KaiLinkedListTest
+    {
+        // Creating a singly linked list that accepts strings.
+        // Primitive data types are basic things that exist.
+        /*
+            Shopping list
+            
+            Bread
+            Milk
+            Cheese
+         
+         */
+        [TestMethod]
+        [DataRow("Toast")]
+
+        public void LinkedListAcceptsStrings_ReturnsStringData(string input)
+        {
+            // arrange
+            KaiLinkedList list = new KaiLinkedList();
+
+            // act
+            list.AddToBack(input);
+            string result = list.Get(0);
+
+            // assert
+            Assert.IsInstanceOfType(result, typeof(string));
+        }
+
+        [TestMethod]
+        [DataRow("Toast")] // Set what comes in and then what it expects.
+        public void LinkedListAcceptsAnItem_HeadReturnsTheItem(string input)
+        {
+            // arrange
+            KaiLinkedList list = new KaiLinkedList();
+            Node expected = new Node(input);
+
+            // act
+            list.AddToBack(input);
+
+            Node result = list.Head;
+
+            // assert
+            Assert.AreEqual(result.Data, expected.Data);
+        }
+
+        [TestMethod]
+        [DataRow("Toast")]
+
+        public void LinkedListAcceptsAnItem_CreatesANodeInTheHead(string input)
+        {
+            // arrange
+            KaiLinkedList list = new KaiLinkedList();
+
+            // act
+            list.AddToBack(input);
+
+            // assert
+            Assert.IsInstanceOfType(list.Head, typeof(Node));
+        }
+
+        [TestMethod]
+
+        public void WhenLinkedListIsCreated_HeadIsNull()
+        {
+            // arrange
+            KaiLinkedList list = new KaiLinkedList();
+
+            // act
+            Node? result = list.Head;
+
+            // assert
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        [DataRow("Foo", "Bar", "Baz")]
+        [DataRow("Toast", "Jelly", "Peanut Butter")]
+
+        public void WhenLinkedListGetsMultipleItems_ItemsAreInCorrectPosition(string one, string two, string three)
+        {
+            // arrange
+            KaiLinkedList list = new KaiLinkedList();
+
+            // act
+            list.AddToBack(one);
+            list.AddToBack(two);
+            list.AddToBack(three);
+
+            string? resultone = list.Get(0);
+            string? resulttwo = list.Get(1);
+            string? resultthree = list.Get(2);
+
+            // assert
+            Assert.AreEqual(resultone, one);
+            Assert.AreEqual(resulttwo, two);
+            Assert.AreEqual(resultthree, three);
+        }
+
+    }
+}
